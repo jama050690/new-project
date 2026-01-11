c879b619a6fd1a74e685e10f015731ba41886843
Jamshiddin <jbm050690@gmail.com>
2026-01-12 00:45:55 +0500

    routerlarni to'g'irladim

--- server.js
+++ server.js
@@ -1,110 +1,95 @@
 import express from "express";
 import ejs from "ejs";
 import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
-app.use("/lib", express.static("lib"));
-app.use("/js", express.static("js"));
-app.use("/css", express.static("css"));
-app.use("/img", express.static("img"));
+// ================= ESM __dirname =================
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// ================= STATIC =================
+app.use("/lib", express.static(path.join(__dirname, "lib")));
+app.use("/js", express.static(path.join(__dirname, "js")));
+app.use("/css", express.static(path.join(__dirname, "css")));
+app.use("/img", express.static(path.join(__dirname, "img")));
+
+// ================= DATA =================
 const dateTable = [
   {
     scope: 1,
     firstname: "Jamshiddin",
     lastname: "Babajonov",
-    email: "jbm050690@gamil.com",
+    email: "jbm050690@gmail.com",
     country: "Uzbekiston",
     zip: 123,
     status: "member",
   },
   {
-    scope: 1,
+    scope: 2,
     firstname: "Tolmas",
     lastname: "O'rinov",
-    email: "jbm050690@gamil.com",
+    email: "tolmas@gmail.com",
     country: "Uzbekiston",
     zip: 456,
     status: "member",
   },
-  {
-    scope: 1,
-    firstname: "Sardor",
-    lastname: "Sunatillayev",
-    email: "jbm050690@gamil.com",
-    country: "Uzbekiston",
-    zip: 789,
-    status: "member",
-  },
 ];
 
-app.get("/index.html", async (req, res) => {
-  const view = await fs.readFile("./src/index.html", "utf8");
-  const home = ejs.render(view, {
-    projectName: "My Project",
-    userName: "Jamshiddin",
-  });
-  res.send(home);
+// ================= HELPER =================
+async function render(res, file, data = {}) {
+  const view = await fs.readFile(path.join(__dirname, "src", file), "utf8");
+
+  res.send(
+    ejs.render(view, {
+      projectName: "My Project",
+      userName: "Jamshiddin",
+      ...data,
+    })
+  );
+}
+
+// ================= ROUTES =================
+
+app.get("/", async (req, res) => {
+  await render(res, "index.html");
 });
-app.get("/404.html", async (req, res) => {
-  const view = await fs.readFile(".//src/404.html", "utf8");
-  const home = ejs.render(view, {
-    projectName: "My Project",
-    userName: "Jamshiddin",
-  });
-  res.send(home);
+
+app.get("/signup", async (req, res) => {
+  await render(res, "signup.html");
 });
-app.get("/blank.html", async (req, res) => {
-  const view = await fs.readFile("./src/blank.html", "utf8");
-  const home = ejs.render(view, {
-    projectName: "My Project",
-    userName: "Jamshiddin",
-  });
-  res.send(home);
+
+app.get("/signin", async (req, res) => {
+  await render(res, "signin.html");
 });
-app.get("/chart.html", async (req, res) => {
-  const view = await fs.readFile("./src/chart.html", "utf8");
-  const home = ejs.render(view, {
-    projectName: "My Project",
-    userName: "Jamshiddin",
-  });
-  res.send(home);
+
+app.get("/blank", async (req, res) => {
+  await render(res, "blank.html");
 });
-app.get("/signin.html", async (req, res) => {
-  const view = await fs.readFile("./src/signin.html", "utf8");
-  const home = ejs.render(view, {
-    projectName: "My Project",
-    userName: "Jamshiddin",
-  });
-  res.send(home);
+
+app.get("/chart", async (req, res) => {
+  await render(res, "chart.html");
 });
-app.get("/signup.html", async (req, res) => {
-  const view = await fs.readFile("./src/signup.html", "utf8");
-  const home = ejs.render(view, {
-    projectName: "My Project",
-    userName: "Jamshiddin",
-  });
-  res.send(home);
+
+app.get("/widget", async (req, res) => {
+  await render(res, "widget.html");
 });
-app.get("/table.html", async (req, res) => {
-  const view = await fs.readFile("./src/table.html", "utf8");
-  const home = ejs.render(view, {
-    projectName: "My Project",
-    userName: "Jamshiddin",
+
+app.get("/table", async (req, res) => {
+  await render(res, "table.html", {
     table: dateTable,
   });
-  res.send(home);
 });
-app.get("/widget.html", async (req, res) => {
-  const view = await fs.readFile("./src/widget.html", "utf8");
-  const home = ejs.render(view, {
-    projectName: "My Project",
-    userName: "Jamshiddin",
-  });
-  res.send(home);
+
+// ================= 404 =================
+app.use(async (req, res) => {
+  await render(res, "404.html");
 });
+
+// ================= START =================
 app.listen(PORT, () => {
-  console.log(`Server running on https://localhost:${PORT}`);
+  console.log(`✅ HTML+EJS server running: http://localhost:${PORT}`);
 });
